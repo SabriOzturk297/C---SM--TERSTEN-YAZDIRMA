95bf16db87e8ecb6262d2e3d3c45d6fd674fe1cd
SabriOzturk297 <119748864+SabriOzturk297@users.noreply.github.com>
2022-12-30 11:05:36 +0300

    Add files via upload

---
+++ c# ismi tersten yazdırma/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Lütfen bir metin giriniz");
+string metin = Console.ReadLine();
+Console.WriteLine("Metnimizin ters hali");
+for (int i = metin.Length - 1; i >= 0; i--)
+{
+    Console.Write(metin[i]);
+}
